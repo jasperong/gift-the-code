912f436e5c76005a442a80d919b59440bade8448
Jasper <jave.ong4@yahoo.com>
2016-10-22 16:43:27 -0400

    Fix redirects

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -5,6 +5,26 @@ class ApplicationController < ActionController::Base
 
   protected
 
+  def after_sign_in_path_for(resource)
+    '/forum'
+  end
+
+  def after_sign_out_path_for(resource_or_scope)
+    request.referrer
+  end
+
+  def after_sign_up_path_for(resource)
+    '/forum'
+  end
+
+  def after_update_path_for(resource)
+    signed_in_root_path(resource)
+  end
+
+  def after_inactive_sign_up_path_for(resource)
+    '/forum' # Or :prefix_to_your_route
+  end
+
   def configure_permitted_parameters
     devise_parameter_sanitizer.permit(:sign_up, keys: [:name, :location, :phone_number, :avatar])
     devise_parameter_sanitizer.permit(:account_update, keys: [:name, :location, :phone_number, :avatar])
