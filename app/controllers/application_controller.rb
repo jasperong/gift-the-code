43831e5e82cd0718b947be357627dd7d692b3115
HankMallard <howardl01@live.ca>
2016-10-22 14:59:07 -0400

    Added Admin column

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,16 +1,16 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
 
-  # before_action :configure_permitted_parameters, if: :devise_controller?
+  before_action :configure_permitted_parameters, if: :devise_controller?
 
   protected
-  #
-  # def configure_permitted_parameters
-  #   devise_parameter_sanitizer.for(:sign_up) << :name
-  #   devise_parameter_sanitizer.for(:sign_up) << :location
-  #   devise_parameter_sanitizer.for(:sign_up) << :phone_number
-  #   devise_parameter_sanitizer.for(:account_update) << :name
-  #   devise_parameter_sanitizer.for(:account_update) << :location
-  #   devise_parameter_sanitizer.for(:account_update) << :phone_number
-  # end
+
+  def configure_permitted_parameters
+    devise_parameter_sanitizer.for(:sign_up) << :name
+    devise_parameter_sanitizer.for(:sign_up) << :location
+    devise_parameter_sanitizer.for(:sign_up) << :phone_number
+    devise_parameter_sanitizer.for(:account_update) << :name
+    devise_parameter_sanitizer.for(:account_update) << :location
+    devise_parameter_sanitizer.for(:account_update) << :phone_number
+  end
 end
