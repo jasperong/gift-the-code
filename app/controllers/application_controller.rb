54bb3bd86dbba34f01e05c8368566d26d331e6b4
Jasper <jave.ong4@yahoo.com>
2016-10-22 13:23:49 -0400

    Modify application_controller

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,12 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
+
+  before_action :configure_permitted_parameters, if: :devise_controller?
+
+  protected
+
+  def configure_permitted_parameters
+    devise_parameter_sanitizer.for(:sign_up) << :name, :location, :phone_number
+    devise_parameter_sanitizer.for(:account_update) << :name, :location, :phone_number
+  end
 end
