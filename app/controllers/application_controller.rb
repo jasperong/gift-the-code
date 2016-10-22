eee17063ac4d9b51f9957123ed988bb429dde3d3
Jasper <jave.ong4@yahoo.com>
2016-10-22 16:00:08 -0400

    Fix avatar bug

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -6,7 +6,7 @@ class ApplicationController < ActionController::Base
   protected
 
   def configure_permitted_parameters
-    devise_parameter_sanitizer.permit(:sign_up, keys: [:name, :location, :phone_number])
-    devise_parameter_sanitizer.permit(:account_update, keys: [:name, :location, :phone_number])
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:name, :location, :phone_number, :avatar])
+    devise_parameter_sanitizer.permit(:account_update, keys: [:name, :location, :phone_number, :avatar])
   end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -246,6 +246,7 @@ ActiveRecord::Schema.define(version: 20161022182903) do
     t.string   "location"
     t.string   "phone_number"
     t.boolean  "admin",                  default: false
+    t.string   "avatar"
     t.index ["email"], name: "index_users_on_email", unique: true, using: :btree
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true, using: :btree
   end
