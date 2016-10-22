fd3897cbe716155190876e3611b6cce34254c155
Jasper <jave.ong4@yahoo.com>
2016-10-22 15:28:09 -0400

    Fix application controller (hopefully)

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -6,11 +6,7 @@ class ApplicationController < ActionController::Base
   protected
 
   def configure_permitted_parameters
-    devise_parameter_sanitizer.for(:sign_up) << :name
-    devise_parameter_sanitizer.for(:sign_up) << :location
-    devise_parameter_sanitizer.for(:sign_up) << :phone_number
-    devise_parameter_sanitizer.for(:account_update) << :name
-    devise_parameter_sanitizer.for(:account_update) << :location
-    devise_parameter_sanitizer.for(:account_update) << :phone_number
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:name, :location, :phone_number])
+    devise_parameter_sanitizer.permit(:account_update, keys: [:name, :location, :phone_number])
   end
 end
