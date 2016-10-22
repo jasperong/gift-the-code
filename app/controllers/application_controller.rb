64a627864f8036d05f84f32d5ee14141b5ea3cf1
Jasper <jave.ong4@yahoo.com>
2016-10-22 13:35:43 -0400

    Reconfigure application controller

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -6,7 +6,11 @@ class ApplicationController < ActionController::Base
   protected
 
   def configure_permitted_parameters
-    devise_parameter_sanitizer.for(:sign_up) << :name, :location, :phone_number
-    devise_parameter_sanitizer.for(:account_update) << :name, :location, :phone_number
+    devise_parameter_sanitizer.for(:sign_up) << :name
+    devise_parameter_sanitizer.for(:sign_up) << :location
+    devise_parameter_sanitizer.for(:sign_up) << :phone_number
+    devise_parameter_sanitizer.for(:account_update) << :name
+    devise_parameter_sanitizer.for(:account_update) << :location
+    devise_parameter_sanitizer.for(:account_update) << :phone_number
   end
 end
